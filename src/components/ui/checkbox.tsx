250815d558038c26abb6b987f49b2de0eaec3349
Aland Baban <amariwan9@icloud.com>
2025-03-01 13:22:56 +0000

    Pending changes exported from your codespace

--- src/components/ui/checkbox.tsx
+++ src/components/ui/checkbox.tsx
@@ -3,11 +3,19 @@ import * as CheckboxPrimitive from "@radix-ui/react-checkbox";
 import { cn } from "@/lib/utils";
 import { Check } from "lucide-react";
 import { Label } from "@/components/ui/label";
+import { cva } from "class-variance-authority";
 
 interface CheckboxWithLabelProps extends React.ComponentPropsWithoutRef<typeof Checkbox> {
   label: React.ReactNode;
   id: string;
 }
+
+export const checkboxVariants = cva(
+  // "  peer h-4 w-4 shrink-0 rounded-sm border border-primary bg-background text-foreground checked:bg-primary checked:text-primary-foreground checked:border-transparent checked:after:content-['✓'] checked:after:flex checked:after:items-center checked:after:justify-center checked:after:h-full checked:after:text-xs checked:after:leading-none checked:after:text-center ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
+  // " peer h-4 w-4 shrink-0 rounded-sm border border-primary ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground"
+  // " peer h-4 w-4 shrink-0 rounded-sm border border-primary ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 checked:bg-primary checked:text-primary-foreground"
+  "peer h-4 w-4 shrink-0 rounded-sm border border-primary ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground"
+);
 
 const Checkbox = React.forwardRef<
   React.ElementRef<typeof CheckboxPrimitive.Root>,
